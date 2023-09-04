ae697ade7cfe2f73da2d66769979ea287a50b386
Ilya (Marshal) <ilya@marshal.dev>
2023-09-05 00:42:23 +0200

    Add lib implementation (cid, car, dag, dab_multi)

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,173 @@
+use std::borrow::Cow;
+use std::collections::{HashMap};
+use std::io::{BufReader, Cursor, Read, Seek};
 use pyo3::prelude::*;
+use pyo3::conversion::ToPyObject;
+use pyo3::{types::PyBool, PyObject, Python};
+use tokio;
+use anyhow::Result;
+use iroh_car::CarReader;
+use futures::stream::StreamExt;
+use ::libipld::cbor::cbor::MajorKind;
+use ::libipld::cbor::decode;
+use ::libipld::multihash::{MultihashDigest};
+use ::libipld::codec::Codec;
+use ::libipld::{cid::Cid, Ipld};
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+
+#[derive(Clone, PartialEq)]
+pub enum HashMapItem {
+    Null,
+    Bool(bool),
+    Integer(i128),
+    Float(f64),
+    String(String),
+    List(Vec<HashMapItem>),
+    Map(HashMap<String, HashMapItem>),
+    Bytes(Cow<'static, [u8]>),
 }
 
-/// A Python module implemented in Rust.
+impl HashMapItem {
+    fn value(&self) -> PyObject {
+        Python::with_gil(|py| match self {
+            Self::Null => PyBool::new(py, false).to_object(py),
+            // FIXME None type support has been added, but not released yet
+            //  ref: https://github.com/PyO3/pyo3/commit/b3cf61cea6c928ca21d3403a046d3ee7b234abc5
+            //  i guess will be included in the next release
+            Self::Bool(b) => b.to_object(py),
+            Self::String(s) => s.to_object(py),
+            Self::Integer(i) => i.to_object(py),
+            Self::Float(f) => f.to_object(py),
+            Self::List(l) => l.to_object(py),
+            Self::Map(m) => m.to_object(py),
+            Self::Bytes(b) => b.to_object(py),
+        })
+    }
+}
+
+impl ToPyObject for HashMapItem {
+    fn to_object(&self, py: Python<'_>) -> PyObject {
+        self.value().into()
+    }
+}
+
+impl IntoPy<Py<PyAny>> for HashMapItem {
+    fn into_py(self, py: Python<'_>) -> Py<PyAny> {
+        self.value().into()
+    }
+}
+
+
+fn ipld_to_hashmap(x: Ipld) -> HashMapItem {
+    match x {
+        Ipld::Null => HashMapItem::Null,
+        Ipld::Bool(b) => HashMapItem::Bool(b),
+        Ipld::Integer(i) => HashMapItem::Integer(i),
+        Ipld::Float(f) => HashMapItem::Float(f),
+        Ipld::String(s) => HashMapItem::String(s),
+        Ipld::Bytes(b) => HashMapItem::Bytes(Cow::Owned(b)),
+        Ipld::List(l) => HashMapItem::List(l.into_iter().map(ipld_to_hashmap).collect()),
+        Ipld::Map(m) => HashMapItem::Map(
+            m.into_iter()
+                .map(|(k, v)| (k, ipld_to_hashmap(v)))
+                .collect(),
+        ),
+        Ipld::Link(cid) => HashMapItem::String(cid.to_string()),
+    }
+}
+
+fn parse_dag_cbor_object<R: Read + Seek>(mut reader: &mut BufReader<R>) -> Result<Ipld> {
+    let major = decode::read_major(&mut reader)?;
+    Ok(match major.kind() {
+        MajorKind::UnsignedInt | MajorKind::NegativeInt => Ipld::Integer(major.info() as i128),
+        MajorKind::ByteString => Ipld::Bytes(decode::read_bytes(&mut reader, major.info() as u64)?),
+        MajorKind::TextString => Ipld::String(decode::read_str(&mut reader, major.info() as u64)?),
+        MajorKind::Array => Ipld::List(decode::read_list(&mut reader, major.info() as u64)?),
+        MajorKind::Map => Ipld::Map(decode::read_map(&mut reader, major.info() as u64)?),
+        MajorKind::Tag => {
+            if major.info() != 42 {
+                return Err(anyhow::anyhow!("non-42 tags are not supported"));
+            }
+
+            parse_dag_cbor_object(reader)?
+        }
+        MajorKind::Other => Ipld::Null,
+    })
+}
+
+#[pyfunction]
+fn decode_dag_multi(data: Vec<u8>) -> PyResult<Vec<HashMapItem>> {
+    let mut reader = BufReader::new(Cursor::new(data));
+
+    let mut parts = Vec::new();
+    loop {
+        let cbor = parse_dag_cbor_object(&mut reader);
+        if let Ok(cbor) = cbor {
+            parts.push(_ipld_to_python(cbor));
+        } else {
+            break;
+        }
+    }
+    Ok(parts)
+}
+
+fn _decode_dag(data: Vec<u8>) -> Result<Ipld> {
+    let mut reader = BufReader::new(Cursor::new(data));
+    parse_dag_cbor_object(&mut reader)
+}
+
+fn _ipld_to_python(ipld: Ipld) -> HashMapItem {
+    ipld_to_hashmap(ipld.clone())
+}
+
+#[pyfunction]
+fn decode_car(data: Vec<u8>) -> HashMap<String, HashMapItem> {
+    let mut runtime = tokio::runtime::Runtime::new().expect("Unable to create a runtime");
+
+    let car = runtime.block_on(CarReader::new(data.as_slice())).unwrap();
+    let records = runtime.block_on(car
+        .stream()
+        .filter_map(|block| async {
+            if let Ok((cid, bytes)) = block {
+                let mut reader = BufReader::new(Cursor::new(bytes));
+
+                let ipld = parse_dag_cbor_object(&mut reader);
+                if let Ok(ipld) = ipld {
+                    Some((cid.to_string(), ipld))
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+        .collect::<HashMap<String, Ipld>>());
+
+    let mut decoded_records = HashMap::new();
+    for (cid, ipld) in &records {
+        decoded_records.insert(cid.to_string(), _ipld_to_python(ipld.clone()));
+    }
+
+    decoded_records
+}
+
+#[pyfunction]
+fn decode_dag(data: Vec<u8>) -> PyResult<HashMapItem> {
+    Ok(_ipld_to_python(_decode_dag(data)?))
+}
+
+#[pyfunction]
+fn decode_cid(data: String) -> PyResult<String> {
+    let cid = Cid::try_from(data.as_str()).unwrap();
+    // TODO - return a proper python object/dict
+    Ok(cid.to_string())
+}
+
 #[pymodule]
 fn libipld(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(decode_cid, m)?)?;
+    m.add_function(wrap_pyfunction!(decode_car, m)?)?;
+    m.add_function(wrap_pyfunction!(decode_dag, m)?)?;
+    m.add_function(wrap_pyfunction!(decode_dag_multi, m)?)?;
     Ok(())
 }
--- test.py
+++ test.py
@@ -1,8 +1,26 @@
+import os.path
+
 import libipld
 
 
 def main():
-    assert libipld.sum_as_string(1, 2) == '3'
+    print(libipld.decode_cid('bafyreig7jbijxpn4lfhvnvyuwf5u5jyhd7begxwyiqe7ingwxycjdqjjoa'))
+
+    for i in range(0, 100):
+        with open(os.path.join('payloads', f'{i}.txt'), 'rb') as f:
+            cbor_parts = libipld.decode_dag_multi(f.read())
+            assert len(cbor_parts) == 2
+            header, body = cbor_parts
+            if header['t'] == '#commit':
+                ops = body['ops']
+                car_file = body['blocks']
+                blocks = libipld.decode_car(car_file)
+                print(ops, blocks)
+
+    for i in range(0, 100):
+        with open(os.path.join('payloads', f'{i}.txt'), 'rb') as f:
+            header = libipld.decode_dag(f.read())
+            print(header)
 
 
 if __name__ == '__main__':
