b5d9ac5365052a3e2392e8a646da427a791b5c38
Ilya (Marshal) <ilya@marshal.dev>
2024-03-02 11:09:57 +0100

    Add IPLD Codec Fixtures

--- pytests/conftest.py
+++ pytests/conftest.py
@@ -3,7 +3,7 @@ import os
 from typing import Any, List, Tuple
 
 
-def load_data_fixtures(dir_path: str) -> List[Tuple[str, Any]]:
+def load_json_data_fixtures(dir_path: str) -> List[Tuple[str, Any]]:
     data = []
     for file in os.listdir(dir_path):
         if not file.endswith('.json'):
@@ -13,3 +13,22 @@ def load_data_fixtures(dir_path: str) -> List[Tuple[str, Any]]:
             data.append((file, json.load(f)))
 
     return data
+
+
+def load_cbor_data_fixtures(dir_path: str) -> List[Tuple[str, Any]]:
+    fixtures = []
+    for root, folder, files in os.walk(dir_path):
+        fixture = {}
+        for file in files:
+            if file in ('.DS_Store',):
+                continue
+
+            file_ext = file.split('.')[-1]
+
+            with open(os.path.join(root, file), 'rb') as f:
+                fixture[file_ext] = f.read()
+
+        if fixture:
+            fixtures.append((os.path.basename(root), fixture))
+
+    return fixtures
